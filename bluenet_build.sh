f358b2c404380300337dbb4e57235c6d905a3b7c
Dominik Egger <dominik@almende.org>
2016-01-28 15:59:28 +0100

    updated configs and build script for sdk8

--- bluenet_build.sh
+++ bluenet_build.sh
@@ -58,72 +58,74 @@ if [ -e "$path/lastCommit.sh" ]; then
 	fi
 fi
 
-mkdir -p "$bluenetConfigsDir/default"
-cp "$bluenetDir/CMakeBuild.config.default" "$bluenetConfigsDir/default/CMakeBuild.config"
+## [28.01.16] don't need to copy over the default anymore, since the default is anyway loaded every time
+##   and overwritten by the CMakeBuild.config in the target directory
+#mkdir -p "$bluenetConfigsDir/default"
+#cp "$bluenetDir/CMakeBuild.config.default" "$bluenetConfigsDir/default/CMakeBuild.config"
 for d in ${bluenetConfigsDir}/* ; do
 	# Set config dir
 	echo "Using "$d" as config dir"
 	export BLUENET_CONFIG_DIR="$d"
-	
+
 	# Remove build dir to be sure we start with a clean build
 	rm -r "$bluenetDir/build"
 	rm -r "$d/build"
-	
+
 	# Clean the logs
 	cd "$logFullDir"
 	rm softdevice*
 	rm firmware*
-	
+
 	# Build the code
 	cd "$bluenetDir/scripts"
 	./softdevice.sh build > "$logFullDir/softdevice_make.log" 2> "$logFullDir/softdevice_make_err.log"
 	checkForError $? "softdevice build"
 	if [ "$?" != "0" ]; then exit 1; fi
-	
+
 	./firmware.sh build crownstone > "$logFullDir/firmware_make.log" 2> "$logFullDir/firmware_make_err.log"
 	checkForError $? "firmware build"
 	if [ "$?" != "0" ]; then exit 1; fi
-	
+
 	# Upload the code
 	cd "$bluenetDir/scripts"
 	./softdevice.sh upload > "$logFullDir/softdevice_upload.log" 2> "$logFullDir/softdevice_upload_err.log"
 	echo "softdevice upload result: $?"
 #	checkForError $? "softdevice upload"
 #	if [ "$?" != "0" ]; then exit 1; fi
-	
+
 	./firmware.sh upload crownstone > "$logFullDir/firmware_upload.log" 2> "$logFullDir/firmware_upload_err.log"
 	checkForError $? "firmware upload"
 	if [ "$?" != "0" ]; then exit 1; fi
-	
+
 	# Give crownstone some time to boot
-	sleep 3
-	
+	sleep 6
+
 	# Read temperature
 	cd "$bleAutomatorDir"
 	./getTemperature.py -i $bluetoothInterface -a $crownstoneAddress > "$logFullDir/read_temperature.log" 2> "$logFullDir/read_temperature_err.log"
 	checkForError $? "read temperature"
 	if [ "$?" != "0" ]; then exit 1; fi
-	
-	
-	
+
+
+
 	# Write some config
 	cd "$bleAutomatorDir"
 	./writeConfig.py -i $bluetoothInterface -a $crownstoneAddress -t 3 -d 5 -n > "$logFullDir/readwrite_config.log" 2> "$logFullDir/readwrite_config_err.log"
 	checkForError $? "write config"
 	if [ "$?" != "0" ]; then exit 1; fi
-	
+
 	# Reset crownstone
 	./reset.py -i $bluetoothInterface -a $crownstoneAddress >> "$logFullDir/readwrite_config.log" 2>> "$logFullDir/readwrite_config_err.log"
 	echo "reset crownstoneresult: $?"
 #	checkForError $? "reset crownstone"
 #	if [ "$?" != "0" ]; then exit 1; fi
 	sleep 10
-	
+
 	# Read config
 	./readConfig.py -i $bluetoothInterface -a $crownstoneAddress -t 3 -n >> "$logFullDir/readwrite_config.log" 2>> "$logFullDir/readwrite_config_err.log"
 	checkForError $? "read config"
 	if [ "$?" != "0" ]; then exit 1; fi
-	
+
 	# Compare written with read config
 	res=0
 	if [ $(grep -c "Value: 5" $logFullDir/readwrite_config.log) -ne 1 ]; then
